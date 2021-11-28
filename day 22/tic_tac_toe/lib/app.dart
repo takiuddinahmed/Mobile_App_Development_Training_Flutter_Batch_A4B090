b9754efa7ff046bfe55b8c15a468ad649857096f
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2021-11-28 17:07:59 +0600

    day 22 part 2

--- day 22/tic_tac_toe/lib/app.dart
+++ day 22/tic_tac_toe/lib/app.dart
@@ -12,21 +12,93 @@ class _GameAppState extends State<GameApp> {
   List<String> data = ["","","","","","","","",""];
 
   String selectedPlayer = "X";
+  Widget winnerText =  Container();
+  bool endGame = false;
 
   userClick(int index){
 
-    if(data[index] == ""){
+    if(data[index] == "" && endGame == false){
       setState(() {
         data[index] = selectedPlayer;
       });
       selectedPlayer = selectedPlayer == "X" ? "O" : "X";
+      checkWin();
+    }
+  }
+
+  String checkEach(int i,int j, int k){
+    String winner = "";
+    if(data[i] != ""){
+      if( data[i] == data[j] && data[i] == data[k]){
+        winner = data[i];
+      }
+    }
+    return winner;
+  }
+
+  Widget gameEndWidget(String text){
+    return Container(
+            margin: EdgeInsets.all(20),
+            child: Row(
+              children : [
+                Text(text, style: TextStyle(color: Colors.redAccent, fontSize: 35,)),
+                SizedBox(width: 20,),
+                ElevatedButton(onPressed: ()=>{
+                  setState(() {
+                    data = ["","","","","","","","",""];
+                    selectedPlayer = "X";
+                    winnerText =  Container();
+                    endGame=false;
+                  })
+                }, child: Text("Reset"))
+                ]
+            ),
+          );
+  }
+
+  checkWin(){
+    var l = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    String winner = "";
+    for(var indexes in l){
+      winner = checkEach(indexes[0], indexes[1], indexes[2]);
+      if(winner != ""){
+        setState(() {
+          endGame = true;
+          winnerText = gameEndWidget("The winner is $winner");
+          
+
+        });
+        break;
+      }
+      else {
+        if(!data.contains("")){
+          setState(() {
+          endGame = true;
+          winnerText = gameEndWidget("Match Draw!!");
+          
+
+        });
+        }
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Table(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        winnerText,
+        Table(
         border: TableBorder.all(),
         children: [
           TableRow(
@@ -51,7 +123,7 @@ class _GameAppState extends State<GameApp> {
             ]
           ),
         ],
-      )
+      )]
     );
   }
 }
